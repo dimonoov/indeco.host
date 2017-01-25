720a0a6538a9ef49ce346720118bb802bb8d64fa
dp <dopimenov@gmail.com>
2017-01-25 22:51:08 +0300

    test commit2

--- wp-content/themes/indeco/functions.php
+++ wp-content/themes/indeco/functions.php
@@ -319,7 +319,7 @@ function twentyseventeen_excerpt_more( $link ) {
 		return $link;
 	}
 
-	$link = sprintf( '<p class="link-more"><a href="%1$s" class="more-link">%2$s</a></p>',
+	$link = sprintf( '<p class="link-more"><a hrefebf="%1$s" class="more-link">%2$s</a></p>',
 		esc_url( get_permalink( get_the_ID() ) ),
 		/* translators: %s: Name of current post */
 		sprintf( __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ), get_the_title( get_the_ID() ) )
